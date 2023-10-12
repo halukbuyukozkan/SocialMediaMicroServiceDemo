b49b78630761d758e3c7df2a1738772693fad9d9
halukbuyukozkanü <halukbuyukozkan@gmail.com>
2023-10-12 14:30:59 +0300

    services registered discovery server

--- post-service/src/main/java/com/buyukozkan/postservice/PostServiceApplication.java
+++ post-service/src/main/java/com/buyukozkan/postservice/PostServiceApplication.java
@@ -2,6 +2,7 @@ package com.buyukozkan.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class PostServiceApplication {
 
