cc23db61d95c6de8fa802e1f5737f5ae2160a0a7
halukbuyukozkanü <halukbuyukozkan@gmail.com>
2023-10-13 11:58:56 +0300

    API Gateaway created and configured

--- api-gateaway/src/main/java/com/buyukozkan/apigateaway/ApiGateawayApplication.java
+++ api-gateaway/src/main/java/com/buyukozkan/apigateaway/ApiGateawayApplication.java
@@ -2,8 +2,10 @@ package com.buyukozkan.apigateaway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGateawayApplication {
 
 	public static void main(String[] args) {
